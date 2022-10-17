baaa3717c8f0b4e1f50ebec1da4e1f832dd0dc5d
Igor Thierry <igorthierry15@gmail.com>
2022-10-17 10:45:35 -0300

    feat: removendo comentários

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -32,6 +32,13 @@ export function Post({ author, content, publishedAt }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(
+      (comment) => comment !== commentToDelete
+    );
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -84,7 +91,11 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => (
-          <Comment key={comment} content={comment} />
+          <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
         ))}
       </div>
     </article>
